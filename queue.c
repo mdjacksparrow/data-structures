8315fddbc03c23255043fd9c449846403d94fbb1
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-07-11 23:20:23 +0530

    added stack and queue in C

---
+++ queue.c
@@ -0,0 +1,70 @@
+/* Queuing using array */
+
+#include<stdio.h>
+#include<conio.h>
+#define MAX 10
+
+int opt, a[20], i, first, last, n;
+
+void create(){
+	printf("\nEnter the limit of the queue: ");
+	scanf("%d", &n);
+	first = 0, last = n - 1;
+	if (n < MAX){
+		for (i = 0; i < n; i++){
+			scanf("%d", & a[i]);
+			last = n - 1;
+		}
+	}
+	else
+		printf("Memory error");
+}
+
+void enq(){
+	int x;
+	if (last < MAX){
+			printf("Enter the element to push:");
+			scanf("%d", &x);
+			last += 1;
+			a[last] = x;
+			n = last;
+	}
+	else
+		printf("Queue limit is reached");
+}
+
+void deq(){
+	if(first > last)
+		printf("Error the queue is empty");
+	else{
+		printf("%d has left the queue", a[first]);
+		first += 1;
+    }
+}
+
+void disp(){
+	printf("The elements are");
+	for(i = first; i <= last; i++){
+		printf(", %d", a[i]);
+	}
+}
+
+
+void main(){
+	while(1){
+//		clrscr();
+		printf("\n == MENU ==");
+		printf("\n 1. Create New \n 2. Enroll \n 3. Leave \n 4. Display \n 0. Exit");
+		printf("\n Enter your choice: ");
+		scanf("%d", &opt);
+		switch(opt){
+			case 1: create(); disp(); break;
+			case 2: enq(); break;
+			case 3: deq(); break;
+			case 4: disp(); break;
+			case 0: exit(0); break;
+			default: printf("\n Invalid input!");
+		}
+	}
+}
+
---
+++ stacks1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <conio.h>
+#define MAX 20
+int opt, a[20], i, top = 0, n;
+void create(){
+	printf("\nEnter the limit of the stack: ");
+	scanf("%d", &n);
+	if (n < MAX){
+		for (i = 0; i < n; i++){
+			scanf("%d", & a[i]);
+		}
+		top = n - 1;
+	}
+	else
+		printf("Memory error");
+}
+
+void display(){
+	if (top < 0){
+		printf("The stack is empty");
+	}
+	else{
+		printf("The elements are");
+		for (i = top; i >= 0; i--){
+			printf(", %d", a[i]);
+		}
+	}
+}
+
+void push(){
+	int x;
+	if (top < MAX){
+		printf("Enter the element to push:");
+		scanf("%d", &x);
+		top += 1;
+		a[top] = x;
+		n = top;
+	}
+	else
+		printf("Stack is full");
+}
+
+void pop(){
+	if (n < 0)
+		printf("The stack is empty");
+	else{
+		printf("Popped element is: %d", a[n]);
+		n -= 1;
+	}
+}
+
+void main(){
+	while(1){
+//		clrscr();
+		printf("\n == MENU ==");
+		printf("\n 1. Create New \n 2. Push \n 3. Pop \n 4. Display \n 0. Exit");
+		printf("\n Enter your choice: ");
+		scanf("%d", &opt);
+		switch(opt){
+			case 1: create(); display(); break;
+			case 2: push(); break;
+			case 3: pop(); break;
+			case 4: display(); break;
+			case 0: exit(0); break;
+			default: printf("\n Invalid input!");
+		}
+	}
+}
+
