f99bf0bdc1ee54a52e22b91cbb52a1a2783077a0
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-07-27 16:57:50 +0530

    bug fixes and file name change

--- stacks.c
+++ stacks.c
@@ -11,19 +11,24 @@ void create(){
 			scanf("%d", & a[i]);
 		}
 		top = n - 1;
+		n -= 1;
 	}
 	else
-		printf("Memory error");
+		printf(" Memory error");
 }
 
 void display(){
 	if (top < 0){
-		printf("The stack is empty");
+		printf(" The stack is empty");
 	}
 	else{
-		printf("The elements are");
+		printf(" The elements are: ");
 		for (i = top; i >= 0; i--){
-			printf(", %d", a[i]);
+			printf("%d", a[i]);
+			if (i != 0)
+				printf(", ");
+			else
+				printf(".");
 		}
 	}
 }
@@ -31,29 +36,30 @@ void display(){
 void push(){
 	int x;
 	if (top < MAX){
-		printf("Enter the element to push:");
+		printf(" Enter the element to push:");
 		scanf("%d", &x);
 		top += 1;
 		a[top] = x;
 		n = top;
 	}
 	else
-		printf("Stack is full");
+		printf(" Stack is full, memory limit reached.");
 }
 
 void pop(){
-	if (n < 0)
-		printf("The stack is empty");
+	if (!(n >= 0))
+		printf(" The stack is empty");
 	else{
-		printf("Popped element is: %d", a[n]);
+		printf(" Popped element is: %d", a[n]);
 		n -= 1;
+		top = n;
 	}
 }
 
-void main(){
+int main(){
 	while(1){
-//		clrscr();
-		printf("\n == MENU ==");
+		// clrscr();
+		printf("\n === MENU ===");
 		printf("\n 1. Create New \n 2. Push \n 3. Pop \n 4. Display \n 0. Exit");
 		printf("\n Enter your choice: ");
 		scanf("%d", &opt);
@@ -62,9 +68,8 @@ void main(){
 			case 2: push(); break;
 			case 3: pop(); break;
 			case 4: display(); break;
-			case 0: exit(0); break;
+			case 0: return;
 			default: printf("\n Invalid input!");
 		}
 	}
 }
-
